ae095f51bf2aff5cf816a0f65ba927838906bba7
talexcrowell <talexcrowell@gmail.com>
2018-10-02 12:15:24 -0700

    added video in new tab function

--- scripts/VideoList.js
+++ scripts/VideoList.js
@@ -1,50 +1,52 @@
 'use strict';
 
 const videoList = (function() {
-    const generateListItem = function(video) {
-        return `
-          <li data-video-id="${video.id}">
-            <h3>${video.title}</h3> 
-            <div>
-              <img src="${video.thumbnail}" alt="${video.title}"/>
-            </div>
-          </li>
-        `;
-    };
+  const generateListItem = function(video) {
+    return `
+      <li class= data-video-id="${video.id}">
+        <h3>${video.title}</h3> 
+        <div>
+          <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank">
+          <img src="${video.thumbnail}" alt="${video.title}"/>
+          </a>
+        </div>
+      </li>
+    `;
+  };
 
-    const decorateResponse = function(response) {
-        return response.items.map( item => {
-          return { 
-            id: item.id.videoId,
-            title: item.snippet.title,
-            thumbnail: item.snippet.thumbnails.default.url  
-          };
-        });
-    };
+  const decorateResponse = function(response) {
+    return response.items.map( item => {
+      return { 
+        id: item.id.videoId,
+        title: item.snippet.title,
+        thumbnail: item.snippet.thumbnails.default.url  
+      };
+    });
+  };
 
-    const render = function() {
-        const search = store.videos.map( video => generateListItem(video));
-        search.join('');
-        $('.results').html(search);
-    };
+  const render = function() {
+    const search = store.videos.map( video => generateListItem(video));
+    search.join('');
+    $('.results').html(search);
+  };
 
-    const handleFormSubmit = function() {
-        $('form').submit(event => {
-          event.preventDefault();
-          let input = $('#search-term').val();
-          $('#search-term').val('');
-          api.fetchVideos(input, response => {
-            const decorated = decorateResponse(response);
-            store.setVideos(decorated);
-            render();
-          });
-        });
-    };
+  const handleFormSubmit = function() {
+    $('form').submit(event => {
+      event.preventDefault();
+      let input = $('#search-term').val();
+      $('#search-term').val('');
+      api.fetchVideos(input, response => {
+        const decorated = decorateResponse(response);
+        store.setVideos(decorated);
+        render();
+      });
+    });
+  };
 
-    const bindEventListeners = function(){
-        handleFormSubmit();
-    }
+  const bindEventListeners = function(){
+    handleFormSubmit();
+  };
 
 
-    return{render, bindEventListeners};
+  return{render, bindEventListeners};
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -7,5 +7,5 @@ const API_KEY = 'AIzaSyDY7ShhZ6_1Y_gf1vOZCocTwEFhjkw-wk0';
 $(function () {
   // TASK:
   // 1. Run `handleFormSubmit` to bind the event listener to the DOM
-   videoList.bindEventListeners();
+  videoList.bindEventListeners();
 });
